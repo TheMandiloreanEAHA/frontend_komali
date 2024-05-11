646c93543ea52f4cc7b024cda051fec680d6a0aa
Marcos Yahir De la caña Pérez <zS20018147@estudiantes.uv.mx>
2024-05-11 12:04:56 -0600

    update home screen

--- src/components/employee/WelcomeScreen.jsx
+++ src/components/employee/WelcomeScreen.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import click from '../../assets/click.svg'
+import click from '../../assets/uv/ICON BTN TOUCH - SVG.svg'
 import logoComedores from '../../assets/uv/logoComedores.svg'
 import fondoMascara from '../../assets/uv/fondoMascara.svg'
 import background from '../../assets/uv/background.jpg'
@@ -15,25 +15,25 @@ const WelcomeScreen = () => {
     useEffect(() => {
         const token = getDataLocalStorage('token')
         const data = jwtDecode(token)
-        if(data){
+        if (data) {
             getDinerRoomData(data.dining_room_id)
         }
-    },[])
+    }, [])
 
     const getDinerRoomData = async (dining_room_id) => {
         const token = getDataLocalStorage('token')
         const url = `http://localhost:8000/dining-room/${dining_room_id}`
         const result = await axiosGet(url, token)
-        if(result !== undefined){
+        if (result !== undefined) {
             setDiningRoomData(result.data)
         }
     }
 
     const setDiningLogo = () => {
         const diningLogo = diningRoomData.dining_logo
-        if(diningLogo === undefined || diningLogo === null){
+        if (diningLogo === undefined || diningLogo === null) {
             return logoComedores
-        }else{
+        } else {
             return diningLogo
         }
     }
@@ -41,9 +41,9 @@ const WelcomeScreen = () => {
     const setDiningBanner = () => {
         const diningBg = diningRoomData.dining_bg
         console.log(diningBg)
-        if(diningBg === undefined || diningBg === null){
+        if (diningBg === undefined || diningBg === null) {
             return background
-        }else{
+        } else {
             return diningBg
         }
     }
@@ -53,31 +53,41 @@ const WelcomeScreen = () => {
     }
 
     return (
-    <>
-        <div className='w-full h-full static'>
-            <div className="bg-white-100 h-96"></div>
-            <div className="bg-white-100 h-96"></div>
-            <div className="bg-white-100 h-12"></div>
-            <div className="flex justify-center items-center w-full mt-8">
-                <div className="w-full relative">
-                    <img src={setDiningBanner()} alt="Background de la facultad" className='w-full'/>
-                    <div className="mt-6 p-4 text-4xl text-white-100 bg-uv-blue rounded-full absolute top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer" onClick={goScanScreen}>
-                        <div className='flex flex-row justify-center items-center px-2'>
-                            <img src={click} alt="Icono click" className="h-10 fill-white-100 stroke-white-100"/>
-                            <span> Toca para comenzar</span>
+        <>
+
+            <div className="relative  h-[96.6vh] overflow-hidden">
+                <img
+                    src={background}
+                    alt="Image 1"
+                    className="absolute inset-0  h-[100vh]l object-fill top-[67vh] bg-cover"
+                />
+                <div className="h-[800vh]"></div>
+                <img
+                    src={fondoMascara}
+                    alt="Image 2"
+                    className="absolute inset-0 w-screen h-[80vh] object-cover "
+                />
+            </div>
+            <div className="fixed inset-0 flex flex-col top-[25vh] items-center text-center z-50">
+                <div>
+                    <h1 className="top-0 text-6xl font-bold">Bienvenido a</h1>
+                    <img
+                        src={logoComedores}
+                        alt="Imágen del sistema"
+                        className="w-65 h-65 object-fill mt-4"
+                    />
+                    <p className="mt-4 text-6xl font-bold">Ordena aquí</p>
+                </div>
+                <div className="mt-8">
+                    <div className="mt-6 p-4 text-4xl text-white-100 bg-uv-blue rounded-full absolute bottom-1 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer" onClick={goScanScreen}>
+                        <div class='flex flex-row justify-center items-center px-1'>
+                            <img src={click} alt="Icono click" className="h-9 fill-white-100 stroke-white-100" />
+                            <span class="ml-5">Toca para comenzar</span>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className='absolute top-12 bg-uv-background bg-cover flex justify-content items-center flex-col pt-36 pb-96 w-full'>
-                <p className="text-7xl">Bienvenido a</p>
-                <div className="bg-black rounded-full py-4">
-                    <img src={setDiningLogo()} alt="Logo comedores" className="w-80 h-80"/>
-                </div>
-                <p className="text-7xl">Ordena Aquí </p>
-            </div>
-        </div>
-    </>
+        </>
     )
 }
 
