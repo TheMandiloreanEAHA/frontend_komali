609fcc8bd58d387f915f14dc5e171d411a217636
TheMandilorianEAHA <errickha@gmail.com>
2024-05-17 10:35:17 -0600

    ModalesCrudAdmin

--- src/components/AdminCrud.jsx
+++ src/components/AdminCrud.jsx
@@ -1,11 +1,19 @@
 import AdminTable from "./AdminTable";
 import NavBarCrud from "./NavBarCrud";
 
-const AdminCrud = () => {
+const AdminCrud = ({ estado, cambiarEstado, fun }) => {
+  const funcion = (accion) => {
+    //console.log(accion);
+    fun(accion);
+  };
   return (
     <>
       <div className="rounded-lg border-4 border-gray-500/20">
-        <NavBarCrud />
+        <NavBarCrud
+          estado={estado}
+          cambiarEstado={cambiarEstado}
+          funcion={funcion}
+        />
         <AdminTable />
       </div>
     </>
