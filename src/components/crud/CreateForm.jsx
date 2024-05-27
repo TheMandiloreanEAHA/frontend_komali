3881fb3444ac47348c18fa985b2761ab5d929dda
Edson Jair Fuentes García <mechcromancer@gmail.com>
2024-05-27 17:50:36 -0600

    [ FEAT ] Funcionamiento de boton delete [ 0.24 ]

--- src/components/crud/CreateForm.jsx
+++ src/components/crud/CreateForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { getDataLocalStorage } from "../../utils/localStorageHelper";
 import { axiosGet, axiosPost } from "../../utils/axiosHelper";
 
--- src/components/crud/DeleteForm.jsx
+++ src/components/crud/DeleteForm.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { useContext } from "react";
+import { crudContext } from "../../pages/Admin";
+import { getDataLocalStorage } from "../../utils/localStorageHelper";
+import { axiosDelete } from "../../utils/axiosHelper";
 
 const DeleteForm = () => {
-  return <div>DeleteForm</div>;
+  const { row, category } = useContext(crudContext);
+  const [selectedRow, setSelectedRow] = row;
+  const [selectedCategory, setSelectedCategory] = category;
+
+  const deleteAdmin = async () => {
+    const token = getDataLocalStorage("token");
+    const url = `http://localhost:8000/${selectedRow}`;
+    const result = await axiosDelete(url, token);
+    if (result !== undefined) {
+      console.log(result);
+    }
+  };
+
+  const deleteDining = async () => {
+    const token = getDataLocalStorage("token");
+    const url = `http://localhost:8000/dining-room/${selectedRow}`;
+    const result = await axiosDelete(url, token);
+    if (result !== undefined) {
+      console.log(result);
+    }
+  };
+
+  const onDeleteRow = () => {
+    switch (selectedCategory) {
+      case "admin":
+        deleteAdmin();
+        break;
+      case "comedores":
+        deleteDining();
+        break;
+      default:
+        console.log("Categoría inválida");
+        break;
+    }
+  };
+
+  return (
+    <div className="w-full h-full my-6 text-xl">
+      <h1 className="font-bold">¿Seguro que desea eliminar este registro?</h1>
+      <button
+        className="px-6 py-2 bg-uv-green text-white-100 rounded-full mt-4"
+        onClick={onDeleteRow}
+      >
+        Confirmar
+      </button>
+    </div>
+  );
 };
 
 export default DeleteForm;
